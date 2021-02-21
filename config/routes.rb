03b3484fd9b0a8bcc731dc95517a1bb91cf03aa9
KantaHasegawa <gyoza20000@gmail.com>
2021-02-21 21:45:15 +0900

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  devise_for :users
+  get 'pages/index'
+  get 'pages/show'
+  root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
