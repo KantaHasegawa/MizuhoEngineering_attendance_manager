8368701bbd74f23c50a0b6a9b2d8d8eeef4cfe6d
KantaHasegawa <gyoza20000@gmail.com>
2021-04-21 21:28:08 +0900

    tableとworking_place.edit.jsのバグ修正

--- app/javascript/packs/working_place/edit.js
+++ app/javascript/packs/working_place/edit.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).ready(function () {
   // 画面表示時にselected以外をdisableにする
   $(".select_form option:not(:selected)").each(function (i, elem) {
     $(elem).prop("disabled", true);
