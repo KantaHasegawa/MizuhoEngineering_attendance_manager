5bc53d3d44a1b31aa44582ca8c029e252e880d92
KantaHasegawa <gyoza20000@gmail.com>
2021-05-21 19:41:14 +0900

    excel変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,22 +18,6 @@ class UsersController < ApplicationController
     @attendances = Attendance.search(@user.id, params[:year], params[:month])
     current_year = params[:year] ||= Date.today.year
     current_month = params[:month] ||= Date.today.month
-    current_month_working_times = 0
-    current_month_overtime_hours = 0
-    current_month_early_attendance = 0
-    current_month_late_leaving = 0
-    current_month_rest_times = 0
-    @attendances.each do |attendance|
-      current_month_working_times += attendance.working_times if attendance.working_times
-      current_month_overtime_hours += attendance.overtime_hours if attendance.overtime_hours
-      current_month_early_attendance += attendance.early_attendance if attendance.early_attendance
-      current_month_late_leaving += attendance.late_leaving if attendance.late_leaving
-      if attendance.rest_times
-        current_month_rest_times += attendance.rest_times
-      end
-    end
-    @current_month_working_times = current_month_working_times.divmod(60)
-    @current_month_overtime_hours = current_month_overtime_hours.divmod(60)
     workbook = RubyXL::Parser.parse("app/assets/template.xlsx")
     workbook.calc_pr.full_calc_on_load = true
     workbook.calc_pr.calc_completed = true
@@ -47,21 +31,17 @@ class UsersController < ApplicationController
       y = attendance.day + 3
       worksheet[y][1].change_contents(attendance.wday)
       worksheet[y][2].change_contents(attendance.working_place)
-      worksheet[y][3].change_contents(attendance.attendance_time.strftime("%H:%M"))
-      worksheet[y][4].change_contents(attendance.leaving_time.strftime("%H:%M"))if attendance.leaving_time
-      worksheet[y][5].change_contents(attendance.working_times) if attendance.working_times
-      worksheet[y][6].change_contents(attendance.early_attendance) if attendance.early_attendance
-      worksheet[y][7].change_contents(attendance.late_leaving) if attendance.late_leaving
-      worksheet[y][8].change_contents(attendance.overtime_hours) if attendance.overtime_hours
+      worksheet[y][3].change_contents(attendance.leaving_place)
+      worksheet[y][4].change_contents(attendance.attendance_time.strftime("%H:%M"))
+      worksheet[y][5].change_contents(attendance.leaving_time.strftime("%H:%M"))if attendance.leaving_time
+      worksheet[y][6].change_contents(attendance.working_times) if attendance.working_times
+      worksheet[y][7].change_contents(attendance.early_attendance) if attendance.early_attendance
+      worksheet[y][8].change_contents(attendance.late_leaving) if attendance.late_leaving
+      worksheet[y][9].change_contents(attendance.overtime_hours) if attendance.overtime_hours
       if attendance.rest_times
-        worksheet[y][9].change_contents(attendance.rest_times)
+        worksheet[y][10].change_contents(attendance.rest_times)
       end
     end
-    worksheet[35][5].change_contents(current_month_working_times)
-    worksheet[35][6].change_contents(current_month_early_attendance)
-    worksheet[35][7].change_contents(current_month_late_leaving)
-    worksheet[35][8].change_contents(current_month_overtime_hours)
-    worksheet[35][9].change_contents(current_month_rest_times)
     respond_to do |format|
       format.html
       format.xlsx do
